93e436dddc38dcbad73f205f786094322b9a4bb0
ParthTanve <parthtanve315@gmail.com>
2025-09-11 15:56:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+import os
+# Ensure you have a .env file with your GENAI_API_KEY or set it as an environment variable.
+GENAI_API_KEY = os.getenv("GENAI_API_KEY", "AIzaSyCwfEryW9hFKJuDwZdJxBaIcbMRKwotetA")
+
+# Add other configurations here if needed
